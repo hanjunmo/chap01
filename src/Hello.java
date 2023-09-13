511db0ebfc5fdbc75fcb3bb73fb440c42552a6b1
sook1281 <sook1281naver.com>
2023-09-13 17:41:21 +0900

    first commit

---
+++ src/Hello.java
@@ -0,0 +1,22 @@
+public class Hello {
+
+       public static void main(String[] args) {
+
+              System.out.println("\033[2J");
+              System.out.println("\033[31m");
+              System.out.println("\033[44m");
+
+              System.out.println("\033[10:20H");
+              System.out.println("Hello Java!");
+ 
+              System.out.println("\033[34m");
+              System.out.println("\033[41m");
+
+              System.out.println("\033[5:10H");
+              System.out.println("Hello JavaScript!");
+
+
+              System.out.println("\033[0m");
+        }
+
+}
